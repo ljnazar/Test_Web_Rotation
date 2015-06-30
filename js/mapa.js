8617cad8373d07a3e5756b214c78ff42166b8955
ljnazar <ljnazar@gmail.com>
2015-06-30 16:06:20 -0300

    mejoras gráficas

---
+++ js/mapa.js
@@ -0,0 +1,21 @@
+function displayMap() {
+    document.getElementById('map').style.display="block";
+    iniciar();
+}
+
+function iniciar() {
+	var mapOptions = {
+		center: new google.maps.LatLng(-32.173320, -64.110574),
+		zoom: 14,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+
+	//marcador con la ubicación de la empresa
+	var place = new google.maps.LatLng(-32.173320, -64.110574);
+	var marker = new google.maps.Marker({
+        position: place
+        , title: 'Oficom SRL'
+        , map: map
+    , });
+}  
--- js/rotar.js
+++ js/rotar.js
@@ -1,23 +1,25 @@
 $(function(){
-	//evento al colocar el puntero del raton sobre el elemento de la lista
+	//evento al colocar el puntero del raton sobre el cartel
 	$('ul.cartel li').hover(function(){
 		//obtenemos la instancia del elemento
 		var $objItem=$(this);
 		//buscamosla imagen y la rotamos 360 grados
-		$objItem.find('.logo').stop(true,true).rotate({
-			            duration: 3000,
+		$objItem.find('.logo1').stop(true,true).rotate({
+			duration: 3000,
 			animateTo: 360
 		});
-		//buscamos la etiqueta ol y la deslizamos hacia abajo
-		$objItem.find('ol').stop(true,true).slideDown('slow');
+		//buscamos la etiqueta y la deslizamos hacia abajo
+		$objItem.find('.datos').stop(true,true).slideDown('slow');
+		//ejecutamos la función que muestra el mapa (anula el display:none en CSS)
+		displayMap();
 	}, //evento al retirar el raton del elemento
 	function(){
 		//obtenemos la instancia del elemento sobre el que esta el raton
 		var $objItem=$(this);
-		//buscamos la lista (ol) y la deslizamos hacia arriba
-		$objItem.find('ol').stop(true,true).slideUp('slow');
+		//buscamos la etiqueta y la deslizamos hacia arriba
+		$objItem.find('.datos').stop(true,true).slideUp('slow');
 		//rotamos la imagen a 0 (cero) grados
-		$objItem.find('.logo').stop(true,true).rotate({
+		$objItem.find('.logo1').stop(true,true).rotate({
 			animateTo: 0
 		});
 	});
