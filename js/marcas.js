8f6474fa88da5c5f7828eb9ea1b6abbece68ab9a
ljnazar <ljnazar@gmail.com>
2015-07-03 14:43:01 -0300

    mejoras visuales y acciones

--- js/marcas.js
+++ js/marcas.js
@@ -1,11 +1,39 @@
 $(function(){
-    //evento al colocar el puntero del raton sobre el texto
+    //evento al colocar el puntero del raton sobre el texto 1
     $('.texto1').hover(function(){
+    	document.getElementById('text1').src = "img/Texto1_activado.png";
         document.getElementById('mark1').style.display="block";
         document.getElementById('mark2').style.display="block";
-    }, //evento al retirar el raton del elemento
+    }, //evento al retirar el raton del texto 1
     function(){
+    	document.getElementById('text1').src = "img/Texto1.png";
         document.getElementById('mark1').style.display="none";
         document.getElementById('mark2').style.display="none";
+    });
+    //evento al colocar el puntero del raton sobre el texto 2
+    $('.texto4').hover(function(){
+    	document.getElementById('text2').src = "img/Texto2_activado.png";
+    	document.getElementById('mark3').style.display="block";
+    }, //evento al retirar el raton del texto 2
+    function(){
+    	document.getElementById('text2').src = "img/Texto2.png";
+    	document.getElementById('mark3').style.display="none";
+    });
+    //evento al colocar el puntero del raton sobre el texto 3
+    $('.texto3').hover(function(){
+    	document.getElementById('text3').src = "img/Texto3_activado.png";
+    	document.getElementById('mark4').style.display="block";
+    	document.getElementById('mark5').style.display="block";
+    	document.getElementById('mark6').style.display="block";
+    	document.getElementById('mark7').style.display="block";
+    	document.getElementById('mark8').style.display="block";
+    }, //evento al retirar el raton del texto 3
+    function(){
+    	document.getElementById('text3').src = "img/Texto3.png";
+    	document.getElementById('mark4').style.display="none";
+    	document.getElementById('mark5').style.display="none";
+    	document.getElementById('mark6').style.display="none";
+    	document.getElementById('mark7').style.display="none";
+    	document.getElementById('mark8').style.display="none";
     });
 });
